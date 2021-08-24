c68296c1c535857ace320ccac3206e7a956948a3
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-24 21:38:58 +0430

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ from pyrogram import filters
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+DB_CHANNEL_ID = "-1001448973320"
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
