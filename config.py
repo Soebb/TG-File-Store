cb11e779d26dd5eb25864c53a06b401e9bdf7f9b
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-24 20:20:28 +0430

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 sudousers = [879491696, 1601268629]
