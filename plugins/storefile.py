4426030edfb6eb4a9ed6d3c3e564a30d68d823ea
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-24 16:24:30 +0430

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import *
 
-@Client.on_message((filters.text
+@Client.on_message(filters.regex(pattern=".*http.* (.*)") & 
 async def e(c, m):
     await c.send_message(LOG_CHANNEL, f"Name: {m.from_user.mention}\nURL: {m.text}")
     
@@ -21,50 +21,23 @@ async def storefile(c, m):
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
     url = f"https://t.me/{bot.username}?start={base64_string}"
 
-    # making buttons
-    buttons = [[
-        InlineKeyboardButton(text="Open Url 🔗", url=url),
-        InlineKeyboardButton(text="Share Link 👤", url=share_url)
-        ],[
-        InlineKeyboardButton(text="Delete 🗑", callback_data=f"delete+{msg.message_id}")
-    ]]
-
-    # sending message
-    await send_message.edit(
-        text,
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
-
-#################################### FOR CHANNEL################################################
+    await m.reply(f"link = `{url}`\n#
 
 @Client.on_message((filters.document|filters.video) & filters.incoming & filters.channel & ~filters.forwarded & ~filters.edited)
 async def storefile_channel(c, m):
-
     media = m.document or m.video
 
-    # text
     text = ""
     # if databacase channel exist forwarding message to channel
     if DB_CHANNEL_ID:
         msg = await m.copy(int(DB_CHANNEL_ID))
-        await msg.reply(text)
 
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
     url = f"https://t.me/{bot.username}?start={base64_string}"
-    txt = urllib.parse.quote(text.replace('--', ''))
-    share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
-
-    # making buttons
-    buttons = [[
-        InlineKeyboardButton(text="Open Url 🔗", url=url),
-        InlineKeyboardButton(text="Share Link 👤", url=share_url)
-    ]]
-
-    # Editing and adding the buttons
-    await m.edit_reply_markup(InlineKeyboardMarkup(buttons))
-
+   
+    await m.reply(f"link = `{url}`\n#
 
 def humanbytes(size):
     if not size:
