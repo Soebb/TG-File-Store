56973269a920212115c5678bb9bd91553f4577f7
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-24 18:32:31 +0430

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -14,7 +14,7 @@ async def e(c, m):
 @Client.on_message((filters.document|filters.video) & sudofilter & filters.incoming & ~filters.edited & ~filters.channel)
 async def storefile(c, m):
     media = m.document or m.video
-    N = media.file.name.replace("@dlmacvin2 - ", "").replace("@dlmacvin - ", "")
+    N = media.file_name.replace("@dlmacvin2 - ", "").replace("@dlmacvin - ", "")
     if DB_CHANNEL_ID:
         msg = await m.copy(int(DB_CHANNEL_ID))
     if 'E0' in N:
@@ -40,25 +40,42 @@ async def storefile(c, m):
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
     url = f"https://t.me/{bot.username}?start={base64_string}"
-
-    await m.reply(f"link = `{url}`\n#
+    s = "#" + n.replace(" ", "_")
+    await m.reply(f"Link: `{url}`\nFile_Name: {N}\nSerial: {s})
 
 @Client.on_message((filters.document|filters.video) & sudofilter & filters.incoming & filters.channel & ~filters.forwarded & ~filters.edited)
 async def storefile_channel(c, m):
     media = m.document or m.video
-
-    text = ""
-    # if databacase channel exist forwarding message to channel
+    N = media.file_name.replace("@dlmacvin2 - ", "").replace("@dlmacvin - ", "")
     if DB_CHANNEL_ID:
         msg = await m.copy(int(DB_CHANNEL_ID))
-
-    # creating urls
+    if 'E0' in N:
+        n = N.split("E0")[0]
+    if 'E1' in N:
+        n = N.split("E1")[0]
+    if 'E2' in N:
+        n = N.split("E2")[0]
+    if 'E3' in N:
+        n = N.split("E3")[0]
+    if 'E4' in N:
+        n = N.split("E4")[0]
+    if 'E5' in N:
+        n = N.split("E5")[0]
+    if 'E6' in N:
+        n = N.split("E6")[0]
+    if 'E7' in N:
+        n = N.split("E7")[0]
+    if 'E8' in N:
+        n = N.split("E8")[0]
+    if 'E9' in N:
+        n = N.split("E9")[0]
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
     url = f"https://t.me/{bot.username}?start={base64_string}"
-   
-    await m.reply(f"link = `{url}`\n#
+    s = "#" + n.replace(" ", "_")
+    await m.reply(f"Link: `{url}`\nFile_Name: {N}\nSerial: {s})
 
+    
 def humanbytes(size):
     if not size:
         return ""
