925f1d860b3e63fa46ac4c401989086e5efbb01a
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-24 13:48:39 +0430

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
